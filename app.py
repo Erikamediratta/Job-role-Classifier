7c7487e3dcc49b0006ed8007b541a0865605482c
Erikamediratta <146512912+Erikamediratta@users.noreply.github.com>
2026-05-25 19:04:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,6 +49,5 @@ def predict():
     return render_template('index.html',
                            prediction=prediction,
                            probability=round(probability, 1))
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
